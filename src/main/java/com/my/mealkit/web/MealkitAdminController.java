350c7d836dcfdb5d156f33e3b7040beec63b6682
서준호 <seojunho>
2022-11-01 15:34:24 +0900

    mealkit

--- src/main/java/com/my/mealkit/web/MealkitAdminController.java
+++ src/main/java/com/my/mealkit/web/MealkitAdminController.java
@@ -1,5 +1,21 @@
 package com.my.mealkit.web;
 
-public class MealkitAdminController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.my.mealkit.service.MealkitService;
+
+@Controller
+@RequestMapping("/admin/mealkit")
+public class MealkitAdminController {
+	@Autowired private MealkitService mealkitService;
+	
+	@Value("${attachPath}") private String attachPath;
+	
+	@RequestMapping("/listMealkit")
+	public String listMealkit() {
+		return "admin/mealkit/listmealkt";
+	}
 }
