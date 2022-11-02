db6c4fd53b2123a4a1f190fb4f983a6e2834988d
박종호 <park@my.com>
2022-11-02 17:02:49 +0900

    11/02

--- src/main/java/com/my/mealkit/web/ReviewAdminController.java
+++ src/main/java/com/my/mealkit/web/ReviewAdminController.java
@@ -1,12 +1,26 @@
 package com.my.mealkit.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.my.mealkit.domain.Review;
+import com.my.mealkit.service.ReviewService;
 
 @Controller 
 @RequestMapping("admin/review")
 public class ReviewAdminController {
+	@Autowired private ReviewService reviewService;
+	
 	@RequestMapping("listReview")
 	public void listReview() {		
+	}
+	
+	@GetMapping("/listReview")
+	public List<Review> getReviews(){
+		return reviewService.getAdminReviews();
 	}
 }
