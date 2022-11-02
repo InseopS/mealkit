28c693443eecc5a35e062cdbf4d12542c4608412
강영민 <m@my.com>
2022-11-02 18:25:54 +0900

    11/02 강영민

--- src/main/java/com/my/mealkit/web/NoticeController.java
+++ src/main/java/com/my/mealkit/web/NoticeController.java
@@ -1,13 +1,18 @@
 package com.my.mealkit.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.my.mealkit.service.NoticeService;
+
 @RestController
 @RequestMapping("notice")
 public class NoticeController {
+	@Autowired private NoticeService noticeService;
+	
 	@GetMapping("listNotice")
 	public ModelAndView listNotice(ModelAndView mv) {
 		mv.setViewName("notice/listNotice");
