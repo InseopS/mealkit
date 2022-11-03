f8a4656cf5f58be45a549fa0a70b44499b981198
박종호 <park@my.com>
2022-11-03 12:25:06 +0900

    11/03 박종호

--- src/main/java/com/my/mealkit/web/ReviewAdminController.java
+++ src/main/java/com/my/mealkit/web/ReviewAdminController.java
@@ -3,24 +3,37 @@ package com.my.mealkit.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.my.mealkit.domain.Review;
 import com.my.mealkit.service.ReviewService;
 
-@Controller 
+@RestController 
 @RequestMapping("admin/review")
 public class ReviewAdminController {
 	@Autowired private ReviewService reviewService;
 	
-	@RequestMapping("listReview")
-	public void listReview() {		
+	@GetMapping("listReview")
+	public ModelAndView list(ModelAndView mv) {
+	   mv.setViewName("admin/review/listReview");
+	   return mv;
 	}
 	
-	@GetMapping("listReview")
+	@PostMapping("getReview")
 	public List<Review> getReviews(){
 		return reviewService.getAdminReviews();
 	}	
+	
+	@ResponseBody
+	@DeleteMapping("del/{reviewNum}")
+	public void delReview(@PathVariable int reviewNum) {
+		reviewService.delReview(reviewNum);
+	}
 }
