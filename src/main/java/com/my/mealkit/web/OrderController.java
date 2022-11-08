d39c1e32ab594b9ddb87760d3b35279434479b8a
김용훈 <star9321@gmail.com>
2022-11-08 10:42:32 +0900

    11/08 김용훈

--- src/main/java/com/my/mealkit/web/OrderController.java
+++ src/main/java/com/my/mealkit/web/OrderController.java
@@ -1,16 +1,27 @@
 package com.my.mealkit.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.my.mealkit.service.OrderService;
+
 @RestController
 @RequestMapping("order")
 public class OrderController {
+	@Autowired private OrderService orederSerivce;
+	
 	@GetMapping("addOrder")
 	 public ModelAndView addOrder(ModelAndView mv) {
 	      mv.setViewName("order/addOrder");
+	      return mv;
+	   }
+	
+	@GetMapping("addOrder2")
+	 public ModelAndView addOrder2(ModelAndView mv) {
+	      mv.setViewName("order/addOrder2");
 	      return mv;
 	   }
 	
