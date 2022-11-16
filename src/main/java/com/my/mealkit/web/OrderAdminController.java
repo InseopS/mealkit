3df13e91d960190df63a13049ab5e3edb76f46db
오연주 <oh@my.com>
2022-11-16 15:26:09 +0900

    11/16 오연주 OrderAdminController 수정

--- src/main/java/com/my/mealkit/web/OrderAdminController.java
+++ src/main/java/com/my/mealkit/web/OrderAdminController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,7 +20,7 @@ import com.my.mealkit.service.OrderService;
 public class OrderAdminController {
 	@Autowired OrderService orderService;
 	
-	@RequestMapping("listOrder")
+	@PostMapping("listOrder")
 	public String listOrder() {
 		return "admin/order/listOrder";
 	}
